fc5513677656edac6ce92ca79c959e9193f9ac41
rahardhikautama <rahardhika@gmail.com>
2025-08-04 21:15:27 -0500

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Paper = {
+  Title: string;
+  Authors: string;
+  DOI: string;
+  Year: number;
+  CitationCount: number;
+  Abstract: string;
+  What_was_studied: string;
+  What_was_found: string;
+  Why_it_matters_for_policy: string;
+  primary_category: string;
+  combined_tags: string;
+  primary_and_combined: string;
+};
